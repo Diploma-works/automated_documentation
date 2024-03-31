5a520ede4530c2deafee265558369e6c8a7076b8
anamrzv <ana.mrzv@mail.ru>
2024-04-01 00:31:57 +0300

    add progress bar and simple walkthrough

--- src/extension.ts
+++ src/extension.ts
@@ -9,50 +9,122 @@ export function activate(context: vscode.ExtensionContext) {
 
 	let disposableGenerate = vscode.commands.registerCommand('automated-documentation-for-java.generateDocumentationFile', async (fileUri) => {
 
-		const javaFileContent = await vscode.workspace.fs.readFile(
-			fileUri
-		);
+		vscode.window.withProgress({
+			location: vscode.ProgressLocation.Notification,
+			title: "Generating documentation for file...",
+			cancellable: true
+		}, (progress, token) => {
+			token.onCancellationRequested(() => {
+				console.log("User canceled the long running operation");
+			});
 
-		const methodsWithContent = getMethodsWithContent(new TextDecoder().decode(javaFileContent));
-		const documentPattern = await createMarkdownDocumentationPattern(methodsWithContent);
-		let rootLink;
-		if (vscode.workspace.workspaceFolders !== undefined) {
-			rootLink = vscode.workspace.workspaceFolders[0].uri.fsPath + '/documentation.md';
-			await vscode.workspace.fs.writeFile(
-				vscode.Uri.parse(rootLink), new TextEncoder().encode(documentPattern)
-			);
-		}
+			progress.report({ increment: 0 });
 
-		vscode.window.showInformationMessage('Documentation file was generated');
+			setTimeout(() => {
+				progress.report({ increment: 10, message: "Still going..." });
+			}, 10000);
+
+			setTimeout(() => {
+				progress.report({ increment: 20, message: "Still going even more..." });
+			}, 20000);
+
+			setTimeout(() => {
+				progress.report({ increment: 20, message: "I am long running!" });
+			}, 30000);
+
+			setTimeout(() => {
+				progress.report({ increment: 20, message: "Almost there..." });
+			}, 40000);
+
+			setTimeout(() => {
+				progress.report({ increment: 20, message: "A bit more time..." });
+			}, 50000);
+
+			const p = new Promise<void>(async resolve => {
+				const javaFileContent = await vscode.workspace.fs.readFile(
+					fileUri
+				);
+
+				const methodsWithContent = getMethodsWithContent(new TextDecoder().decode(javaFileContent));
+				const documentPattern = await createMarkdownDocumentationPattern(methodsWithContent);
+				let rootLink;
+				if (vscode.workspace.workspaceFolders !== undefined) {
+					rootLink = vscode.workspace.workspaceFolders[0].uri.fsPath + '/documentation.md';
+					await vscode.workspace.fs.writeFile(
+						vscode.Uri.parse(rootLink), new TextEncoder().encode(documentPattern)
+					);
+				}
+
+				vscode.window.showInformationMessage('Documentation file was generated!');
+				resolve();
+			});
+			return p;
+		});
 	});
 
 	let disposableLoad = vscode.commands.registerCommand('automated-documentation-for-java.loadProjectToMemory', async (dirUri) => {
 
-		await vscode.workspace.fs.copy(
-			dirUri,
-			vscode.Uri.parse(`${__dirname}/sourcecode`)
-		);
+		vscode.window.withProgress({
+			location: vscode.ProgressLocation.Notification,
+			title: "Loading folder to memory...",
+			cancellable: true
+		}, (progress, token) => {
+			token.onCancellationRequested(() => {
+				console.log("User canceled the long running operation");
+			});
 
-		try {
-			await loadSourceCodeFilesToVector(`${__dirname}/sourcecode`);
-			await vscode.workspace.fs.delete(
-				vscode.Uri.parse(`${__dirname}/sourcecode`),
-				{
-					recursive: true
-				}
-			);
-				vscode.window.showInformationMessage('Folder was loaded to memory');
-		} catch (error) {
-			await vscode.workspace.fs.delete(
-				vscode.Uri.parse(`${__dirname}/sourcecode`),
-				{
-					recursive: true
-				}
-			);
-			vscode.window.showErrorMessage('Error while executing command');
-		}
+			progress.report({ increment: 0 });
 
+			setTimeout(() => {
+				progress.report({ increment: 10, message: "Still going..." });
+			}, 10000);
+
+			setTimeout(() => {
+				progress.report({ increment: 20, message: "Still going even more..." });
+			}, 20000);
+
+			setTimeout(() => {
+				progress.report({ increment: 20, message: "I am long running!" });
+			}, 30000);
+
+			setTimeout(() => {
+				progress.report({ increment: 20, message: "Almost there..." });
+			}, 40000);
+
+			setTimeout(() => {
+				progress.report({ increment: 20, message: "A bit more time..." });
+			}, 50000);
+
+			const p = new Promise<void>(async resolve => {
+				await vscode.workspace.fs.copy(
+					dirUri,
+					vscode.Uri.parse(`${__dirname}/sourcecode`)
+				);
 		
+				try {
+					await loadSourceCodeFilesToVector(`${__dirname}/sourcecode`);
+					await vscode.workspace.fs.delete(
+						vscode.Uri.parse(`${__dirname}/sourcecode`),
+						{
+							recursive: true
+						}
+					);
+					vscode.window.showInformationMessage('Folder was loaded to memory');
+				} catch (error) {
+					await vscode.workspace.fs.delete(
+						vscode.Uri.parse(`${__dirname}/sourcecode`),
+						{
+							recursive: true
+						}
+					);
+					vscode.window.showErrorMessage('Error while executing command');
+				}
+
+				resolve();
+			});
+			return p;
+		});
+
 	});
 
 	context.subscriptions.push(disposableGenerate, disposableLoad);
